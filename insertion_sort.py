26712c33021ae8c26b21e3fac5b6fd0ba89831d7
SteffenJoergensen <crz@crz.dk>
2021-02-17 13:35:55 +0100

    Corrections to Insertion Sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -4,27 +4,25 @@ INSERTION SORT
 Description:
 Remembers an element, right-shifts everything greater than the given element once,
 and places said element in the correct position. The array is thus split into two;
-a sorted and an unsorted part. This is done for all elements.
+a sorted and an unsorted part (the invariant). This is done for all elements.
 
-Time complexity: O(n^2)
-The iteration happens in O(n), and the shifting of elements in O(n) (worst case).
+Worst case running time: Theta(n^2)
+The iteration happens in Theta(n), and the shifting of elements in Theta(n).
 """
 
 import numpy as np
 
-array = np.random.randint(0, 10, size=5)
-print("Unsorted: ", array)
+A = np.random.randint(0, 10, size=5)
+print("Unsorted: ", A)
 
-def insertion_sort():
-	for i in range(1, len(array)): # Iterate over all elements but first
-		current = array[i] # Remember
+for i in range(1, len(A)): # Iterate over all elements but first
+	key = A[i] # Remember
 
-		j = i - 1
-		while j >= 0 and array[j] > current: # Right-shift greater elements of sorted part
-			array[j+1] = array[j]
-			j -= 1
+	j = i - 1
+	while j >= 0 and A[j] > key: # Right-shift greater elements of sorted part
+		A[j+1] = A[j]
+		j -= 1
 
-		array[j+1] = current # Place
+	A[j+1] = key # Place
 
-insertion_sort()
-print("Sorted: ", array)
+print("Sorted: ", A)
